f7d48fd1bbb6dc0592cfcdb0275985456a7a760f
mr-aman01 <123452520+mr-aman01@users.noreply.github.com>
2024-07-05 10:15:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+'''
 #class Config:
 BOT_TOKENS:[    
     "5883625169:AAFIDjQpmiKMsHfBpyQD5Hhe6XrpmcT2R5U",
@@ -12,3 +13,26 @@ AUTH_USERS:[
     "1937616968",
     "5454197638"
 ]
+'''
+
+# config.py
+
+# Bot tokens for four different bots
+BOT_TOKENS = [
+    "5883625169:AAFIDjQpmiKMsHfBpyQD5Hhe6XrpmcT2R5U",
+    "6217604158:AAGoZDhR9QU8snasj_Gl2ZkUVqxG3E8NJS8",
+    "5688285331:AAHJBF38L8bat_jMrgidNaaWT9PO2RVfaqs",
+    "6206927174:AAHG4UdeVe5oh9Lgjo5IQeiQ3cw1kxyxqwE"
+]
+
+# Telegram API credentials
+API_ID = "20145727"
+API_HASH = "d9bb45ffcd4b965c515a714f2680f4a7"
+
+# Authorized users (optional)
+AUTH_USERS = [
+    "1937616968",
+    "5454197638"
+]
+
+# Add more configuration variables here if needed
